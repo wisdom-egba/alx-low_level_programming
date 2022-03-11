112c24c2217d90ae287d3bfb66899d26d2e4041c
wiz-js <egbawisdom34@gmail,com>
2022-03-11 05:55:48 -0800

    my codes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Prints the alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
