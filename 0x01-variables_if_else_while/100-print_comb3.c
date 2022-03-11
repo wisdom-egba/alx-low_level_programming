c20b87302944cd842c1e6faa485016e49a322757
wiz-js <egbawisdom34@gmail,com>
2022-03-11 06:54:00 -0800

    mycodes

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - program that prints numbers from 0 to 99 without figure repeat.
+ * Return: 0
+ */
+
+int main(void)
+{
+int i, j;
+
+i = 0;
+j = 0;
+
+while (i < 9)
+{
+j = i + 1;
+while (j < 10)
+{
+putchar('0' + i);
+putchar('0' + j);
+
+if (i < 8 || j < 9)
+{
+putchar(',');
+putchar(' ')
+}
+j++;
+}
+++i;
+}
+
+putchar('\n');
+
+return (0);
+}
