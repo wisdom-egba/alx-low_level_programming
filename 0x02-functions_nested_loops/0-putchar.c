a5db9de5b8ab25411314b8b8b5594292fee067f4
wiz-js <egbawisdom34@gmail,com>
2022-03-15 17:11:35 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - Print the word "Holberton"
+ * main - prints _putchar
  *
- * Return: Always 0 (Success)
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-int i;
 char text[9] = "_putchar";
+int i = 0;
 
 for (i = 0; i < 8; i++)
 {
-_putchar(p[i]);
+_putchar(text[i]);
 }
 _putchar('\n');
+
 return (0);
 }
