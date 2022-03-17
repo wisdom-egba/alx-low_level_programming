634632eb29307d9bb8eca88287c46f7d44f9f678
wiz-js <egbawisdom34@gmail,com>
2022-03-17 05:41:25 -0700

    good programme

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: Variable text
- * Return: Always 0.
+ * _isupper - checks for uppercase
+ * @c: character to checked for case
+ *
+ * Return: 1 if uppercase, 0 if not uppercase
  */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-{
 return (1);
-}
-else
-{
 return (0);
-}
 }
