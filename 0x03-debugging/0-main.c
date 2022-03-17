d478be99a54d7101e60e458d0f0e1aa1d138a8d7
wiz-js <egbawisdom34@gmail,com>
2022-03-17 06:38:26 -0700

    trojan

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <main.h>
 
 /**
@@ -12,18 +10,10 @@
  */
 
 int main(void)
-{
-int n;
+{int i;
 
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is positive\n", n);
+i = 0;
+positive_or_negative(i);
 
 return (0);
 }
