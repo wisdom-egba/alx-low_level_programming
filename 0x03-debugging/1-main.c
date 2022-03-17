b53dd87fee7aa87800c3f2e79653b601f8b71851
wiz-js <egbawisdom34@gmail,com>
2022-03-17 06:51:00 -0700

    programme

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,10 @@
 carrie@ubuntu:/debugging$ cat 1-main.c
 #include <stdio.h>
 
-	       /**
-		* main - causes an infinite loop
-		* Return: 0
-		*/
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -14,10 +14,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
+/*while (i < 10)
 {
 putchar(i);
-}
+}*/
 
 printf("Infinite loop avoided! \\o/\n");
 
